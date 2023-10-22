62071689c6b1fe70100cfd60f44b396b635cc7d2
Ben Brady <benbradybusiness@gmail.com>
2023-10-22 17:36:23 +0100

    0.2.1

--- tempered/__init__.py
+++ tempered/__init__.py
@@ -1,5 +1,5 @@
 "Generate native python functions from HTML templates"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 from .main import (
     add_template, add_template_folder,
     register_type, build, load,
