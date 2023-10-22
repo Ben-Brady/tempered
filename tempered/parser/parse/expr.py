fe6ddf657dd5a380a84b3061e621fb405320c8e8
Ben Brady <benbradybusiness@gmail.com>
2023-10-22 18:32:31 +0100

    Rearranged parameter match statement

--- tempered/parser/parse/expr.py
+++ tempered/parser/parse/expr.py
@@ -15,14 +15,6 @@ def parse_parameter(parameter: str) -> TemplateParameter:
             value=ast.Name(id=name)
         ):
             return TemplateParameter(name=name)
-        case ast.Assign(
-            targets=[ast.Name(id=name)],
-            value=ast.Constant(value=default)
-        ):
-            return TemplateParameter(
-                name=name,
-                default=default
-            )
         case ast.AnnAssign(
             target=ast.Name(id=name),
             annotation=ast.Name(id=type)|ast.Constant(value=type),
@@ -32,6 +24,14 @@ def parse_parameter(parameter: str) -> TemplateParameter:
                 name=name,
                 type=type if isinstance(type, str) else type.id,
                 default=default if default is not None else RequiredParameter()
+            )
+        case ast.Assign(
+            targets=[ast.Name(id=name)],
+            value=ast.Constant(value=default)
+        ):
+            return TemplateParameter(
+                name=name,
+                default=default
             )
         case ast.AnnAssign(
             target=ast.Name(id=name),
