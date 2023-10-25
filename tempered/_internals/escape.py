e46a9c4e74ae74f3179d3acf635edf83eceea030
Ben Brady <benbradybusiness@gmail.com>
2023-10-25 18:01:11 +0100

    Added cache to escape

--- tempered/_internals/escape.py
+++ tempered/_internals/escape.py
@@ -1,5 +1,8 @@
 from markupsafe import escape_silent
 from typing import Any
+from functools import lru_cache
 
+
+@lru_cache(maxsize=10_000)
 def escape(value: Any) -> str:
     return str(escape_silent(value))
