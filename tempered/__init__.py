32db7ba14dde3d24c593a5c44a4f852a01a27899
Ben Brady <benbradybusiness@gmail.com>
2023-10-27 14:04:22 +0100

    Improved Lexer Error Messages

--- tempered/__init__.py
+++ tempered/__init__.py
@@ -1,5 +1,5 @@
 "Generate native python functions from HTML templates"
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 from .main import (
     add_template, add_template_folder, register_type,
     build, build_to, build_static,
