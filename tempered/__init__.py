4f68401df8f0359d471e3190e81d71729c0b1b99
Ben Brady <benbradybusiness@gmail.com>
2023-11-22 13:05:24 +0000

    Exporting ParserException

--- tempered/__init__.py
+++ tempered/__init__.py
@@ -1,7 +1,10 @@
 "Generate native python functions from HTML templates"
 __version__ = "0.6.4"
 from .tempered import Tempered
+from .errors import ParserException
+
 
 __all__ = [
-    "Tempered"
+    "Tempered",
+    "ParserException",
 ]
