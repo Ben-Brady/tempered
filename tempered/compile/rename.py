7283c42c941b97ccedcf90b0528f0a7f1ab9506a
Ben Brady <benbradybusiness@gmail.com>
2023-11-23 19:13:35 +0000

    Fixed built-ins being replaced

--- tempered/compile/rename.py
+++ tempered/compile/rename.py
@@ -1,18 +1,22 @@
 import ast
 
+
 def convert_unknown_variables_to_kwargs(body: list[ast.stmt], known_names: list[str]):
     class NameTransformer(ast.NodeTransformer):
         def visit_Name(self, node: ast.Name):
             return transform(node)
 
     def transform(node: ast.Name) -> ast.expr:
-        if any((
-            node.id == "kwargs",
-            node.id.startswith("__"),
-            node.id in dir(__builtins__),
-            node.id in known_names,
-        )):
+        if any(
+            (
+                node.id == "kwargs",
+                node.id.startswith("__"),
+                node.id in known_names,
+                is_builtin(node.id),
+            )
+        ):
             return node
+
 
         return ast.Subscript(
             value=ast.Name(id="kwargs", ctx=ast.Load()),
@@ -20,7 +24,18 @@ def convert_unknown_variables_to_kwargs(body: list[ast.stmt], known_names: list[
             ctx=getattr(node, "ctx", ast.Load()),
         )
 
-
     transformer = NameTransformer()
     for node in body:
         transformer.visit(node)
+
+
+def is_builtin(name: str):
+    if name in dir(__builtins__):
+        return True
+
+    try:
+        eval(name, {}, {})
+    except NameError:
+        return False
+    else:
+        return True
