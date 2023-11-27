1fa881ad6db3e2f4140fca432567dec232805035
Ben Brady <benbradybusiness@gmail.com>
2023-11-27 22:00:54 +0000

    Split up pop in text_scanner

--- tempered/parser/text_scanner.py
+++ tempered/parser/text_scanner.py
@@ -7,13 +7,13 @@ from pathlib import Path
 class TextScanner:
     original: str
     position: int = 0
-    file: Path|None
+    file: Path | None
 
     if TYPE_CHECKING:
         text: array[str]
         _checkpoint: tuple[array[str], int] | None = None
 
-    def __init__(self, html: str, file: Path|None = None):
+    def __init__(self, html: str, file: Path | None = None):
         self.file = file
         self.original = html
         self.text = array("u")
@@ -33,37 +33,47 @@ class TextScanner:
         self.text, self.position = self._checkpoint
         self._checkpoint = None
 
-    def pop(self, length: int = 1) -> str:
-        if len(self.text) < length:
-            self.error("Unexpected end of text")
-
-        popped_text = ""
-        for _ in range(length):
-            popped_text += self.text.pop()
-
-        self.position += length
-        return popped_text
+    def pop(
+        self,
+    ) -> str:
+        return self.text.pop()
 
     def accept(self, *text: str) -> bool:
         for match in text:
             if not self.startswith(match):
                 continue
 
-            self.text = self.text[:-len(match)]
+            self.text = self.text[: -len(match)]
             self.position += len(match)
             return True
 
         return False
 
+    def expect(self, text: str):
+        if not self.accept(text):
+            raise self.error(f"Expected {text!r}")
+
     def startswith(self, *text: str) -> bool:
         for match in text:
-            html_text = self.text[-len(match) : :][::-1].tounicode()
-            if html_text == match:
+            prefix = self.text[-len(match) : :][::-1].tounicode()
+
+            if prefix == match:
                 return True
 
         return False
 
-    def take_optional(self, *text: str) -> str|None:
+    def pop_many(self, length: int) -> str:
+        if len(self.text) < length:
+            self.error("Unexpected end of text")
+
+        popped_text = ""
+        for _ in range(length):
+            popped_text += self.pop()
+
+        self.position += length
+        return popped_text
+
+    def take_optional(self, *text: str) -> str | None:
         for match in text:
             if not self.startswith(match):
                 continue
@@ -73,19 +83,12 @@ class TextScanner:
 
         return None
 
-
     def take(self, *text: str) -> str:
         value = self.take_optional(*text)
         if value is None:
             raise self.error(f"Expected one of {','.join(text)}")
 
         return value
-
-
-
-    def expect(self, text: str):
-        if not self.accept(text):
-            raise self.error(f"Expected {text!r}")
 
     def take_until(self, matches: str | list[str]) -> str:
         if isinstance(matches, str):
@@ -112,4 +115,3 @@ class TextScanner:
 
     def error(self, msg: str) -> Exception:
         return ParserException.create(msg, self.file, self.original, self.position)
-
