60ef56b50a297cb1aecd2669b80e4533140f0914
Ben Brady <benbradybusiness@gmail.com>
2023-11-29 14:11:37 +0000

    Updated escape cache

--- tempered/_internals/escape.py
+++ tempered/_internals/escape.py
@@ -3,6 +3,6 @@ from typing import Any
 from functools import lru_cache
 
 
-@lru_cache(maxsize=10_000)
+@lru_cache(maxsize=1024, typed=True)
 def escape(value: Any) -> str:
     return str(escape_silent(value))
