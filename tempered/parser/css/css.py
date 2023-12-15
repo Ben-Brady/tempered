dbf3a3317b985425b870a359ad173e5cc5cc6d6f
Ben Brady <benbradybusiness@gmail.com>
2023-12-15 00:22:21 +0000

    Renamed css scoped

--- tempered/parser/css/css.py
+++ tempered/parser/css/css.py
@@ -20,9 +20,8 @@ def extract_css(body: str, prefix: str = "tempered") -> ExtractedCss:
 
     style_tags = cast(list[bs4.Tag], soup.find_all("style"))
     for tag in style_tags:
-        scope_id = scoped._generate_scoped_style_id(prefix)
+        scope_id = scoped.generate_scope_id(prefix)
         scoped.apply_scope_to_soup(soup, scope_id)
-
         css = tag.text
         is_global = tag.has_attr("global")
         lang = tag.get("lang", None)
