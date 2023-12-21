3164066771f64f311d6a9541707e91d80db6ec59
Ben Brady <benbradybusiness@gmail.com>
2023-12-21 15:25:29 +0000

    python3.9 uses markupsafe

--- tempered/_internals/escape.py
+++ tempered/_internals/escape.py
@@ -1,16 +1,27 @@
 from typing import Any
-
+import sys
 
 SAFE_CONVERSIONS = (int, float)
 
-def escape(value: Any) -> str:
-    if type(value) in SAFE_CONVERSIONS:
-        return str(value)
-    return (
-        str(value)
-        .replace("&", "&amp;")
-        .replace(">", "&gt;")
-        .replace("<", "&lt;")
-        .replace("'", "&#39;")
-        .replace('"', "&#34;")
-    )
+if sys.version_info <= (3, 9):
+    def escape(value: Any) -> str:
+        if type(value) in SAFE_CONVERSIONS:
+            return str(value)
+
+        return (
+            str(value)
+            .replace("&", "&amp;")
+            .replace(">", "&gt;")
+            .replace("<", "&lt;")
+            .replace("'", "&#39;")
+            .replace('"', "&#34;")
+        )
+
+else:
+    from markupsafe import escape_silent
+
+    def escape(value: Any) -> str:
+        if type(value) in SAFE_CONVERSIONS:
+            return str(value)
+
+        return str(escape_silent(value))
