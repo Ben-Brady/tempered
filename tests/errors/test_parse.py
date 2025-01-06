8a0b77904d16d46910abd92bba5766cbe79a8683
Ben Brady <benbrady04.bb@gmail.com>
2025-01-06 00:51:22 +0000

    fix(test_parse): removed invalid test

--- tests/errors/test_parse.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import tempered
-from tempered.src.template.template import parse_template
-
-
-def test_invalid_end_block():
-    with pytest.raises(tempered.InvalidTemplateException):
-        parse_template("foo", "<t:block name='test'></t:block>")
