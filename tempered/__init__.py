377bb3be35a3625d7bc4401cd563ae09b2e2083a
Ben Brady <benbrady04.bb@gmail.com>
2025-01-06 01:23:23 +0000

    build(1.11.0):  updated version

--- tempered/__init__.py
+++ tempered/__init__.py
@@ -1,5 +1,5 @@
 "Generate native python functions from HTML templates"
-__version__ = "0.10.0"
+__version__ = "0.11.0"
 from .src.errors import (
     InvalidTemplateException as InvalidTemplateException,
     ParserException as ParserException,
