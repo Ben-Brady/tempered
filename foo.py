40831860df16aad63e949b1e0e5753e047a1307b
Ben Brady <benbrady04.bb@gmail.com>
2025-01-06 01:05:49 +0000

    build: removed foo.py

--- foo.py
+++
@@ -1,8 +0,0 @@
-from tempered import Tempered
-from tempered.src.utils.soup import HtmlSoup
-
-tempered = Tempered()
-tempered.add_from_file("./foo.html")
-html = tempered.render("foo.html", fade=True)
-
-print(HtmlSoup(html).decode(pretty_print=True))
