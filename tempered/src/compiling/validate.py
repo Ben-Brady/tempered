b77e7ff7d81c6eef3ea7a23e6ff7c2daa4264486
Ben Brady <benbrady04.bb@gmail.com>
2025-01-06 00:56:01 +0000

    feat(Validate): added validation for component imports

--- tempered/src/compiling/validate.py
+++ tempered/src/compiling/validate.py
@@ -6,11 +6,35 @@ from ..parsing.nodes import LayoutTemplate, Template
 def validate_templates(templates: t.Sequence[Template]):
     template_lookup = {template.name: template for template in templates}
     for template in templates:
+        check_for_invalid_imports(template, template_lookup)
+        check_for_missing_component_import(template, template_lookup)
         check_for_missing_layout(template, template_lookup)
         check_for_missing_blocks(template, template_lookup)
         check_for_non_existant_blocks(template, template_lookup)
         check_for_duplicate_parameters(template)
 
+
+def check_for_invalid_imports(template: Template, lookup: t.Dict[str, Template]):
+    for _import in template.imports:
+        import_name = _import.name
+        if import_name not in lookup:
+            raise InvalidTemplateException.create(
+                msg=f"Imported component {import_name} could not be found",
+                name=template.name,
+                file=template.file,
+            )
+
+def check_for_missing_component_import(template: Template, lookup: t.Dict[str, Template]):
+    import_names = [x.target for x in template.imports]
+
+    for call in template.components_calls:
+        name = call.component_name
+        if name not in import_names:
+            raise InvalidTemplateException.create(
+                msg=f"Component <t:{name} not imported",
+                name=template.name,
+                file=template.file,
+            )
 
 def check_for_missing_layout(template: Template, lookup: t.Dict[str, Template]):
     if template.layout is None:
