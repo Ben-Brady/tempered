3591d383278bd340beee9889fe58d680b29c7dec
Ben Brady <benbrady04.bb@gmail.com>
2025-01-18 17:24:27 +0000

    other: removed benchmarks

--- benchmarks/throughput/run.py
+++
@@ -1,9 +0,0 @@
-
-import os, sys
-from pathlib import Path
-import subprocess
-
-def run():
-    os.environ["PYTHONPATH"] = str(Path(__file__).parent.parent)
-    process = subprocess.Popen([sys.executable, "throughput/servers/tempered_server.py"])
-    # from servers import tempered_server
