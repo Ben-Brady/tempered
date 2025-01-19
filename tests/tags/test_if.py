339e47945d8225780ce4788ff7b2e9a952280f39
Ben Brady <benbrady04.bb@gmail.com>
2025-01-19 15:53:30 +0000

    test: add more elif tests

--- tests/tags/test_if.py
+++ tests/tags/test_if.py
@@ -48,6 +48,57 @@ def test_elif_block():
     assert "a" in component(expr_a=True, expr_b=False)
     assert "b" in component(expr_a=False, expr_b=True)
 
+def test_elif_and_else_block():
+    component = build_template(
+        """
+        <ul>
+
+        <t:if condition="a">
+            a
+        </t:if>
+        <t:elif condition="b">
+            b
+        </t:elif>
+        <t:else>
+            c
+        </t:else>
+        </ul>
+    """
+    )
+    assert "a" in component(a=True, b=True)
+    assert "a" in component(a=True, b=False)
+    assert "b" in component(a=False, b=True)
+    assert "c" in component(a=False, b=False)
+
+
+def test_multiple_elif_and_else_block():
+    component = build_template(
+        """
+        <t:if condition="a">
+            a
+        </t:if>
+        <t:elif condition="b">
+            b
+        </t:elif>
+        <t:elif condition="c">
+            c
+        </t:elif>
+        <t:else>
+            d
+        </t:else>
+    """
+    )
+    assert "a" in component(a=True, b=True, c=True)
+    assert "a" in component(a=True, b=True, c=False)
+    assert "a" in component(a=True, b=False, c=True)
+    assert "a" in component(a=True, b=False, c=False)
+
+    assert "b" in component(a=False, b=True, c=False)
+    assert "b" in component(a=False, b=True, c=True)
+
+    assert "c" in component(a=False, b=False, c=True)
+    assert "d" in component(a=False, b=False, c=False)
+
 
 def test_empty_if_block():
     component = build_template('<t:if condition="True"></t:if>')
